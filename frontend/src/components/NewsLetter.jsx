7e4f30303a9da472f622ed0c4c96f441ef177d38
Ahmad Ali <ahmad.ali619x@gmail.com>
2025-08-16 19:21:39 +0500

    worked on newsletter, footer, and blog page.

--- frontend/src/components/NewsLetter.jsx
+++ frontend/src/components/NewsLetter.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 const NewsLetter = () => {
     return (
-        <div>
-            Newsletter
+        <div className='flex flex-col items-center justify-center space-y-2 my-32'>
+            <h1 className='md:text-4xl text-2xl font-semibold'>Never Miss a Blog!</h1>
+            <p className='md:text-lg text-gray-500/70 pb-8'>Subscribe to get the latest blog, new tech, and exclusive news.</p>
+
+            <form className='flex items-center justify-between max-w-2xl w-full md:h-13 h-12'>
+                <input
+                className='border border-gray-300 rounded-md h-full border-r-0 w-full outline-none rounded-r-none text-gray-500 px-3'
+                    placeholder='Enter your email id'
+                    required
+                    type="text" />
+                <button
+                type='submit'
+                    className='h-full md:px-12 px-8 text-white bg-primary/80 hover:bg-primary transition-all cursor-pointer rounded-md rounded-l-none'
+                >Subscribe</button>
+            </form>
         </div>
     )
 }
--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { assets, blog_data } from '../assets/assets'
+import Navbar from '../components/Navbar'
+import Moment from 'moment'
 
 const Blog = () => {
-    return (
-        <div>
-            Blog
+    const { id } = useParams()
+    const [data, setData] = useState(null)
+
+    const fetchingBlogData = async () => {
+        const data = blog_data.find(item => item._id === id)
+        setData(data)
+    }
+
+    useEffect(() => {
+        fetchingBlogData()
+    }, [])
+
+    return data ?  (
+        <div className='relative'>
+            <img 
+            className='absolute -top-50 -z-1 opacity-50'
+            src={assets.gradientBackground} 
+            alt="image" />
+
+            <Navbar />
+
+            <div className='text-center mt-20 text-gray-600'>
+                <p className='text-primary py-4 font-medium'>Published on {Moment(data.createdAt).format('MMMM Do YYYY')}</p>
+                <h1 className='text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto text-gray-800'>{data.title}</h1>
+                <h2 className='my-5 max-w-lg mx-auto truncate'>{data.subTitle}</h2>
+                <p className='inline-block border border-primary/35 bg-primary/5 py-1 px-4 mb-6 rounded-full text-sm font-medium text-primary'>Michael Brown</p>
+            </div>
+
+            <div className='mx-5 max-w-5xl md:mx-auto my-10 mt-6'>
+                <img 
+                className='rounded-3xl mb-5'
+                src={data.image} 
+                alt="blog image" />
+
+                <div dangerouslySetInnerHTML={{__html: data.description}}>
+
+                </div>
+            </div>
         </div>
-    )
+    ) : <div>
+        loading...
+    </div>
 }
 
 export default Blog
