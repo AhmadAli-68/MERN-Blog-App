91a6fc2a5bcd7bba824c756c340403b91fe414e1
Ahmad Ali <ahmad.ali619x@gmail.com>
2025-08-21 22:23:55 +0500

    Working on dashboard data

---
+++ frontend/src/components/admin/Sidebar.jsx
@@ -0,0 +1,39 @@
+import { NavLink } from 'react-router-dom'
+import { assets } from '../../assets/assets'
+
+const Sidebar = () => {
+    return (
+        <div className='flex flex-col border-r border-gray-200 min-h-full pt-6'>
+            <NavLink
+                className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-64 cursor-pointer ${isActive && "bg-primary/10 border-r-4 border-primary"}`}
+                end={true}
+                to={"/admin"}>
+                <img src={assets.home_icon} alt="home icon" className='min-w-4 w-5' />
+                <p className='hidden md:inline-block'>Dashboard</p>
+            </NavLink>
+
+            <NavLink
+                className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-64 cursor-pointer ${isActive && "bg-primary/10 border-r-4 border-primary"}`}
+                to="/admin/addBlog">
+                <img src={assets.add_icon} alt="home icon" className='min-w-4 w-5' />
+                <p className='hidden md:inline-block'>Add Blog</p>
+            </NavLink>
+
+            <NavLink
+                className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-64 cursor-pointer ${isActive && "bg-primary/10 border-r-4 border-primary"}`}
+                to="/admin/listBlog">
+                <img src={assets.list_icon} alt="home icon" className='min-w-4 w-5' />
+                <p className='hidden md:inline-block'>Blog Lists</p>
+            </NavLink>
+
+            <NavLink
+                className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-64 cursor-pointer ${isActive && "bg-primary/10 border-r-4 border-primary"}`}
+                to="/admin/comments">
+                <img src={assets.comment_icon} alt="home icon" className='min-w-4 w-5' />
+                <p className='hidden md:inline-block'>Comments</p>
+            </NavLink>
+        </div>
+    )
+}
+
+export default Sidebar
--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { assets, dashboard_data } from '../../assets/assets'
 
 const Dashboard = () => {
+    const [dashboardData, setDashboardData] = useState({
+        blogs: 0,
+        comments: 0,
+        drafts: 0,
+        recentBlogs: []
+    })
+
+    const fetchDashboard = async () => {
+        setDashboardData(dashboard_data)
+    }
+
+    useEffect(() => {
+        fetchDashboard()
+    }, [])
+
     return (
-        <div>
-            Dashboard
+        <div className='flex-1 p-5 md:p-10 bg-blue-50/50'>
+            <div className='flex flex-wrap gap-4'>
+                <div className='flex items-center gap-4 p-4 bg-white min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all'>
+                    <img src={assets.dashboard_icon_1} alt="dashboard icon" className='' />
+                    <div>
+                        <p className='font-semibold text-xl text-gray-600'>{dashboardData.blogs}</p>
+                        <p className='text-gray-400 font-light'>Blogs</p>
+                    </div>
+                </div>
+
+                <div className='flex items-center gap-4 p-4 bg-white min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all'>
+                    <img src={assets.dashboard_icon_2} alt="dashboard icon" className='' />
+                    <div>
+                        <p className='font-semibold text-xl text-gray-600'>{dashboardData.comments}</p>
+                        <p className='text-gray-400 font-light'>Comments</p>
+                    </div>
+                </div>
+
+                <div className='flex items-center gap-4 p-4 bg-white min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all'>
+                    <img src={assets.dashboard_icon_3} alt="dashboard icon" className='' />
+                    <div>
+                        <p className='font-semibold text-xl text-gray-600'>{dashboardData.drafts}</p>
+                        <p className='text-gray-400 font-light'>Drafts</p>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <div className='flex items-center gap-3 m-4 mt-6 text-gray-600'>
+                    <img src={assets.dashboard_icon_4} alt="dashboard" />
+                    <p>Latest Blogs</p>
+                </div>
+
+                <div className=''>
+                    <table className=''>
+                        <thead className=''>
+                            <tr>
+                                
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
         </div>
     )
 }
