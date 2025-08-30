5ede3dbd51d49fc70f0ed55c4e8151ac273d7a75
Ahmad Ali <ahmad.ali619x@gmail.com>
2025-08-30 18:10:47 +0500

    Completed the add blog section and finished the frontend

--- frontend/src/pages/admin/AddBlog.jsx
+++ frontend/src/pages/admin/AddBlog.jsx
@@ -1,10 +1,97 @@
-import React from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { assets, blogCategories } from '../../assets/assets'
+import Quill from 'quill'
 
 const AddBlog = () => {
+    const [image, setImage] = useState(false)
+    const [title, setTitle] = useState("")
+    const [subtitle, setSubtitle] = useState("")
+    const [category, setCategory] = useState("Startup")
+    const [isPublished, setIsPublished] = useState(false)
+
+    const editorRef = useRef(null)
+    const quillRef = useRef(null)
+
+    const onSubmitHandler = async (e) => {
+        e.preventDefault()
+    }
+
+    const generateContent = async () => {
+
+    }
+
+    useEffect(() => {
+        // Initiate Quill only once
+        if (!quillRef.current && editorRef.current) {
+            quillRef.current = new Quill(editorRef.current, { theme: "snow" })
+        }
+    }, [])
+
     return (
-        <div>
-            Add Blog
-        </div>
+        <form onSubmit={onSubmitHandler} className='flex-1 bg-blue-50/50 text-gray-600 h-full overflow-scroll'>
+            <div className="bg-white w-full max-w-3xl p-4 md:p-10 sm:m-10 shadow rounded">
+                <p>Upload thumbnail</p>
+                <label htmlFor="image">
+                    <img src={!image ? assets.upload_area : URL.createObjectURL(image)} alt="upload area" className='mt-2 h-16 rounded cursor-pointer' />
+                    <input
+                        onChange={(e) => setImage(e.target.files[0])}
+                        type="file"
+                        id="image" hidden required />
+                </label>
+
+                <p className='mt-4'>Blog Title</p>
+                <input
+                    type="text"
+                    placeholder='Type here'
+                    className='w-full max-w-lg mt-2 p-2 border border-gray-300 outline-none rounded'
+                    onChange={e => setTitle(e.target.value)}
+                    value={title}
+                    required />
+
+                <p className='mt-4'>Subtitle</p>
+                <input
+                    type="text"
+                    placeholder='Type here'
+                    className='w-full max-w-lg mt-2 p-2 border border-gray-300 outline-none rounded'
+                    onChange={e => setSubtitle(e.target.value)}
+                    value={subtitle}
+                    required />
+
+                <p className='mt-4'>Blog Description</p>
+                <div className="max-w-lg h-74 pb-16 sm:pb-10 pt-2 relative">
+                    <div ref={editorRef}></div>
+
+                    <button
+                        onClick={generateContent}
+                        className='absolute right-2 bottom-1 ml-2 text-xs text-white bg-black/70 px-4 py-1.5 rounded hover:underline cursor-pointer'
+                        type='button'>Generate with AI</button>
+                </div>
+
+                <p className='mt-4'>Blog Category</p>
+                <select
+                    onChange={e => setCategory(e.target.value)}
+                    className='mt-2 px-3 border py-2 text-gray-500 outline-none border-gray-300 rounded'
+                    name="category">
+                    <option value="">Select Category</option>
+                    {blogCategories.map((item, index) => {
+                        return <option value={item} key={index}>{item}</option>
+                    })}
+                </select>
+
+                <div className='flex mt-4 gap-2'>
+                    <p>Publish Now</p>
+                    <input
+                        type="checkbox"
+                        className='scale-125 cursor-pointer'
+                        onChange={e => setIsPublished(e.target.value)}
+                        checked={isPublished} />
+                </div>
+
+                <button 
+                className='w-40 mt-8 h-10 bg-primary text-white rounded cursor-pointer text-sm'
+                type='submit'>Add Blog</button>
+            </div>
+        </form>
     )
 }
 
