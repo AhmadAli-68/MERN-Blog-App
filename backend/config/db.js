70eb5935fa8f689bbc2508c04942a2c721ac4616
Ahmad Ali <ahmad.ali619x@gmail.com>
2025-08-31 23:43:49 +0500

    Changes in the db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const connectDB = async () => {
     try {
-        mongoose.connection.on("connected", () => console.log("MongoDB is connected.")
-        )
-        await mongoose.connect(process.env.MONGODB_URI)
+        await mongoose.connect(process.env.MONGODB_URI, {})
+        console.log("MongoDB is connected successfully.");
     } catch (error) {
         console.log(error.message);
     }
