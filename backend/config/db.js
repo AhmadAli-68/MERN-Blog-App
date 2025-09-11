74bb1073c35d3d901a2a863528bf99290f38ccc5
Ahmad Ali <ahmad.ali619x@gmail.com>
2025-09-11 15:48:09 +0500

    changes in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,10 @@ dotenv.config()
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {})
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
         console.log("MongoDB is connected successfully.");
     } catch (error) {
         console.log("Error connecting to MongoDB", error.message);
